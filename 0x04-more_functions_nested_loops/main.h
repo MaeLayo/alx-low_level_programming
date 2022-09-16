4aa37deadc346b45c101d64a72715486f0e721cb
MaeLayo <layomiomoadewoye@gmail.com>
2022-09-16 06:42:31 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
 
 void print_numbers(void);
 void print_most_numbers(void);
@@ -19,7 +19,5 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
-
-
 
 #endif /* MAIN_H */
